332365b1948900726d035b613b86a50b19b48c90
Newton <newtonbfjr@gmail.com>
2022-08-02 18:43:40 -0300

    adição de imagem do perfil

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -1,6 +1,9 @@
 const perfilController = {
     perfil: (req, res)=>{
         res.render('perfil')
+    }, 
+    salvarImagem:(req,res)=>{
+        
     }
 }
 
--- src/routes/perfil.routes.js
+++ src/routes/perfil.routes.js
@@ -1,8 +1,24 @@
 const { Router } = require('express')
 const perfilController = require('../controllers/perfilController')
-
+const path = require('path')
+const multer = require ('multer')
 const perfilRouter = Router()
 
-perfilRouter.get('/perfil', perfilController.perfil)
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback)=> {
+        let folder = path.join(__dirname,'../public/profile-image')
+        callback(null,folder)
+    },
+    filename: (req,file,callback)=>{
+        let imageName = Date.now() + path.extname(file.originalname)
+        callback(null,imageName)
+    }
+})
 
+const upload = multer({ storage: multerDiskStorage })
+
+
+perfilRouter.get('/perfil', perfilController.perfil)
+perfilRouter.post('/perfil',upload.single(''),
+ perfilController.salvarImagem)
 module.exports = perfilRouter
