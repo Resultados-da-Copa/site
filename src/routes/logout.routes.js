c459a7ec33a0477d5b8e19e5ffbed886b8fa09c7
Joao Pedro Gobain <joaopedrogobain@gmail.com>
2022-10-13 17:05:45 -0400

    -Creatin conditional to render links on header if user is logged.

--- src/routes/logout.routes.js
+++ src/routes/logout.routes.js
@@ -3,5 +3,6 @@ const controller = require('../controllers/logoutController')
 const logoutRouter = Router()
 
 logoutRouter.post('/logout', controller.logout)
+logoutRouter.get('/logout', controller.logout)
 
 module.exports = logoutRouter
