1ee28b60a7e4cc5fb7c2581b87a9583fd3100a3d
Joao Pedro Gobain <joaopedrogobain@gmail.com>
2022-10-17 22:54:38 -0400

    -Creating more seeders.

---
+++ src/database/seeders/20221018005202-offside.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { v4: uuid } = require('uuid');
+const { team, matches, players } = require('../../database');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('offsides', [{
+      
+      id: uuid(),
+      id_player: await players.findOne({
+        where: {
+          name: 'richarlison'
+        }
+      }).then(result => {
+        return result.dataValues.id
+      }),
+      id_match: await matches.findOne({
+        where: {
+          stopped_match: 1
+        }
+      }).then(result => {
+        return result.dataValues.id
+      }),
+      id_team: await team.findOne({
+        where: {
+          name: 'brasil'
+        }
+      }).then(result => {
+        return result.dataValues.id
+      }),
+      minute: 40,
+      createdAt: new Date()
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('offsides', null, {});
+  }
+};
